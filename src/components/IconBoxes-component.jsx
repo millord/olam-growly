8460813b1e5fed180fa852747fce410fd2b3491c
millord <juancarlosmillord@gmail.com>
2020-04-29 10:50:31 -0400

    fixed the icons

--- src/components/IconBoxes-component.jsx
+++ src/components/IconBoxes-component.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "materialize-css";
-import { Container, Row, Card, Section ,Col} from "react-materialize";
+import { Container, Row, Card, Section, Col } from "react-materialize";
 
 export const IconBoxes = () => {
   return (
@@ -9,22 +9,22 @@ export const IconBoxes = () => {
         <Row>
           <Col s={12} m={4}>
             <Card>
-              <i className="material-icons  large teal-text">room</i>
-              <h4>Pick Where</h4>
+              <i className="material-icons  large teal-text">terrain</i>
+              <h4>Obten el mejor aceite</h4>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Card>
           </Col>
           <Col s={12} m={4}>
             <Card>
-              <i className="material-icons  large teal-text">room</i>
-              <h4>Pick Where</h4>
+              <i className="material-icons  large teal-text">spa</i>
+              <h4>Aceites de cualquier albor</h4>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Card>
           </Col>
           <Col s={12} m={4}>
             <Card>
-              <i className="material-icons  large teal-text">room</i>
-              <h4>Pick Where</h4>
+              <i className="material-icons  large teal-text">filter_vintage</i>
+              <h4>Mejores cosechas</h4>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Card>
           </Col>
